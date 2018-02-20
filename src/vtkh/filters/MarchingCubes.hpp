b2f2881f791b0a52df6bb4f1f097f296ad974145
Matt Larsen <larsen30@llnl.gov>
2018-02-20 08:38:38 -0800

    adding levels as an alternative to iso-values

--- src/vtkh/filters/MarchingCubes.hpp
+++ src/vtkh/filters/MarchingCubes.hpp
@@ -15,7 +15,7 @@ public:
   virtual ~MarchingCubes(); 
   void SetIsoValue(const double &iso_value);
   void SetIsoValues(const double *iso_values, const int &num_values);
-
+  void SetLevels(const int &levels);
   void SetField(const std::string &field_name);
 
 protected:
@@ -26,6 +26,7 @@ protected:
 
   std::vector<double> m_iso_values;
   std::string m_field_name;
+  int m_levels;
 };
 
 } //namespace vtkh
