e37e115621f2a6c515b635882cb60d69411765d0
Matt Larsen <larsen30@llnl.gov>
2018-04-10 15:50:16 -0700

    fixing arg order

--- scripts/uberenv/uberenv.py
+++ scripts/uberenv/uberenv.py
@@ -419,9 +419,10 @@ def main():
         install_cmd = "spack/bin/spack "
         if opts["ignore_ssl_errors"]:
             install_cmd += "-k "
+        install_cmd += "install "
         if opts["jobs"]:
-            install_cmd += "-j" + opts["jobs"] + " "
-        install_cmd += "install " + uberenv_pkg_name + opts["spec"]
+            install_cmd += "-j " + opts["jobs"] + " "
+        install_cmd += uberenv_pkg_name + opts["spec"]
         res = sexe(install_cmd, echo=True)
         if res != 0:
             return res
