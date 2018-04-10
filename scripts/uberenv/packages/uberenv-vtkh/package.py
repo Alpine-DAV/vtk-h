299d25003e28a3234eb190376f2e84c4251890d5
Matt Larsen <larsen30@llnl.gov>
2018-04-09 20:29:18 -0700

    adding better uber support and adding no_op examples

--- scripts/uberenv/packages/uberenv-vtkh/package.py
+++ scripts/uberenv/packages/uberenv-vtkh/package.py
@@ -49,35 +49,18 @@ import os
 import platform
 from os.path import join as pjoin
 
+from vtkh import Vtkh
 
-def cmake_cache_entry(name,value):
-    return 'set("%s" "%s" CACHE PATH "")\n\n' % (name,value)
-        
-
-
-class UberenvVtkh(Package):
+class UberenvVtkh(Vtkh):
     """Spack Based Uberenv Build for VTK-h Thirdparty Libs """
 
-    homepage = "http://example.com"
+    homepage = "http://github.com/alpine-DAV/vtk-h"
 
     version('0.1', '8d378ef62dedc2df5db447b029b71200')
     
     # would like to use these in the future, but we need global variant support
     #variant('cuda',   default=False, description="Enable CUDA support.")
     #variant('openmp', default=False, description="Enable OpenMP support.")
-
-    variant("mpich",default=False,description="build mpich as MPI lib for Alpine")
-    
-    depends_on("cmake@3.8.2")
-
-    depends_on("vtkm@1.2.0")
-    
-
-    #on osx, always build mpich for mpi support
-    if "darwin" in platform.system().lower():
-        depends_on("mpich")
-    else: # else, defer to the variant
-        depends_on("mpich",when="+mpich")
 
     def url_for_version(self, version):
         dummy_tar_path =  os.path.abspath(pjoin(os.path.split(__file__)[0]))
@@ -86,119 +69,11 @@ class UberenvVtkh(Package):
         return url
         
     def install(self, spec, prefix):
-        dest_dir     = env["SPACK_DEBUG_LOG_DIR"]
-
-        c_compiler   = env["SPACK_CC"]
-        cpp_compiler = env["SPACK_CXX"]
-
-        sys_type = spec.architecture
-        # if on llnl systems, we can use the SYS_TYPE
-        if env.has_key("SYS_TYPE"):
-            sys_type = env["SYS_TYPE"]
-        
-        #######################
-        # TPL Paths
-        #######################
-        cmake_exe  = pjoin(spec['cmake'].prefix.bin,"cmake")
-
-        print "cmake executable: %s" % cmake_exe
-        
-        #######################
-        # Check for MPI
-        #######################
-        mpicc   = which("mpicc")
-        mpicxx  = which("mpicxx")
-        mpif90  = which("mpif90")
-        mpiexec = which("mpiexec")
-        nvcc    = which("nvcc")
-        
-        if not nvcc is None:
-            enable_cuda = "ON"
-        else:
-            enable_cuda = "OFF"
-
-
-        #######################
-        # Create host-config
-        #######################
-        host_cfg_fname = "%s-%s-%s.cmake" % (socket.gethostname(),sys_type,spec.compiler)
-        host_cfg_fname = pjoin(dest_dir,host_cfg_fname)
-
-        cfg = open(host_cfg_fname,"w")
-        cfg.write("##################################\n")
-        cfg.write("# uberenv host-config\n")
-        cfg.write("##################################\n")
-        cfg.write("# %s-%s\n" % (sys_type,spec.compiler))
-        cfg.write("##################################\n\n")
-        # show path to cmake for reference
-        cfg.write("# cmake from uberenv\n")
-        if cmake_exe is None:
-            cfg.write("# cmake not built by uberenv\n\n");
-        else:
-            cfg.write("# cmake executable path: %s\n\n" % cmake_exe)
-        
-        #######################
-        #######################
-        # compiler settings
-        #######################
-        #######################
-        
-        cfg.write("#######\n")
-        cfg.write("# using %s compiler spec\n" % spec.compiler)
-        cfg.write("#######\n\n")
-        cfg.write("# c compiler used by spack\n")
-        cfg.write(cmake_cache_entry("CMAKE_C_COMPILER",c_compiler))
-        cfg.write("# cpp compiler used by spack\n")
-        cfg.write(cmake_cache_entry("CMAKE_CXX_COMPILER",cpp_compiler))
-
-
-        #######################
-        # mpi
-        #######################
-        cfg.write("# MPI Support\n")
-        if not mpicc is None:
-            cfg.write(cmake_cache_entry("ENABLE_MPI","ON"))
-            cfg.write(cmake_cache_entry("MPI_C_COMPILER",mpicc.command))
-        else:
-            cfg.write(cmake_cache_entry("ENABLE_MPI","OFF"))
-
-        # we use `mpicc` as `MPI_CXX_COMPILER` b/c we don't want to introduce 
-        # linking deps to the MPI C++ libs (we aren't using C++ features of MPI)
-        if not mpicxx is None:
-            cfg.write(cmake_cache_entry("MPI_CXX_COMPILER",mpicc.command))
-        if not mpif90 is None:
-            cfg.write(cmake_cache_entry("MPI_Fortran_COMPILER", mpif90.command))
-        if not mpiexec is None:
-            cfg.write(cmake_cache_entry("MPIEXEC", mpiexec.command))
-
-        #######################
-        # cuda
-        #######################
-        cfg.write("# CUDA support\n")
-        cfg.write(cmake_cache_entry('ENABLE_CUDA',enable_cuda));
-        if not nvcc is None:
-            cfg.write(cmake_cache_entry('CUDA_BIN_DIR',os.path.dirname(nvcc.command)))
-   
-        #######################
-        # vtkm + tpls
-        #######################
-
-        cfg.write("\n# vtkm support\n\n")    
-
-        cfg.write("# tbb from uberenv\n")
-        cfg.write(cmake_cache_entry("TBB_DIR", spec['tbb'].prefix))
-
-        cfg.write("# vtkm from uberenv\n")
-        cfg.write(cmake_cache_entry("VTKM_DIR", spec['vtkm'].prefix))
-
-
-        cfg.write("##################################\n")
-        cfg.write("# end uberenv host-config\n")
-        cfg.write("##################################\n")
-
-        cfg.close()
-        mkdirp(prefix)
-        install(host_cfg_fname,prefix)
-        print "[result host-config file: %s]" % host_cfg_fname
-
-        
+        """
+        Build and install vtk-h
+        """
+        with working_dir('spack-build', create=True):
+          host_cfg_fname = self.create_host_config(spec, prefix)
+          mkdirp(prefix)
+          install(host_cfg_fname,prefix)
+          install(host_cfg_fname,env["SPACK_DEBUG_LOG_DIR"])
