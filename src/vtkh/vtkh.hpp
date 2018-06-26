1dc864e55c6d9df2bc1c790505fbad32debc8f2a
Cyrus Harrison <cyrush@llnl.gov>
2018-06-26 15:41:21 -0700

    unconfuse vtkh vs vtkh_mpi

--- src/vtkh/vtkh.hpp
+++ src/vtkh/vtkh.hpp
@@ -7,16 +7,20 @@ namespace vtkh
 {
 
   std::string AboutVTKH();
-  bool IsSerialEnabled();
-  bool IsOpenMPEnabled();
-  bool IsCUDAEnabled();
-  void ForceSerial();
-  void ForceOpenMP();
-  void ForceCUDA();
-  void ResetDevices();
-  int GetMPIRank();
-  int GetMPISize();
-  void SetMPICommHandle(int mpi_comm_id);
-  int  GetMPICommHandle();
+  bool        IsSerialEnabled();
+  bool        IsOpenMPEnabled();
+  bool        IsCUDAEnabled();
+  bool        IsMPIEnabled();
+  
+  void        ForceSerial();
+  void        ForceOpenMP();
+  void        ForceCUDA();
+  void        ResetDevices();
+
+  int         GetMPIRank();
+  int         GetMPISize();
+  
+  void        SetMPICommHandle(int mpi_comm_id);
+  int         GetMPICommHandle();
 }
 #endif
