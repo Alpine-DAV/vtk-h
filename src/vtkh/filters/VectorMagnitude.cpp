211197be89476b9928f2cba90bc0fa3f709cd282
mclarsen <larsen30@llnl.gov>
2018-09-17 10:27:13 -0700

    adding vector magnitude

---
+++ src/vtkh/filters/VectorMagnitude.cpp
@@ -0,0 +1,75 @@
+#include <vtkh/Error.hpp>
+#include <vtkh/filters/VectorMagnitude.hpp>
+
+#include <vtkm/filter/VectorMagnitude.h>
+
+namespace vtkh 
+{
+
+VectorMagnitude::VectorMagnitude()
+{
+
+}
+
+VectorMagnitude::~VectorMagnitude()
+{
+
+}
+
+void 
+VectorMagnitude::SetField(const std::string &field_name)
+{
+  m_field_name = field_name;
+}
+
+void 
+VectorMagnitude::SetResultName(const std::string name)
+{
+  m_out_name = name;
+}
+
+void VectorMagnitude::PreExecute() 
+{
+  Filter::PreExecute();
+  assert(m_field_name != "");
+
+  if(m_out_name == "")
+  {
+    m_out_name = m_field_name + "_magnitude";
+  }
+}
+
+void VectorMagnitude::PostExecute()
+{
+  Filter::PostExecute();
+}
+
+void VectorMagnitude::DoExecute()
+{
+  this->m_output = new DataSet();
+  const int num_domains = this->m_input->GetNumberOfDomains();
+  
+  for(int i = 0; i < num_domains; ++i)
+  {
+    vtkm::Id domain_id;
+    vtkm::cont::DataSet dom;
+    this->m_input->GetDomain(i, dom, domain_id);
+   
+    vtkm::filter::VectorMagnitude mag;
+    mag.SetOutputFieldName(m_out_name);
+    mag.SetFieldsToPass(this->GetFieldSelection());
+    mag.SetActiveField(m_field_name);
+
+    auto dataset = mag.Execute(dom);
+    
+    m_output->AddDomain(dataset, domain_id);
+  }
+}
+
+std::string
+VectorMagnitude::GetName() const
+{
+  return "vtkh::VectorMagnitude";
+}
+
+} //  namespace vtkh
---
+++ src/vtkh/filters/VectorMagnitude.hpp
@@ -0,0 +1,30 @@
+#ifndef VTK_H_VECTOR_MAGNITUDE_HPP
+#define VTK_H_VECTOR_MAGNITUDE_HPP
+
+#include <vtkh/vtkh.hpp>
+#include <vtkh/filters/Filter.hpp>
+#include <vtkh/DataSet.hpp>
+
+namespace vtkh
+{
+
+class VectorMagnitude : public Filter
+{
+public:
+  VectorMagnitude(); 
+  virtual ~VectorMagnitude(); 
+  std::string GetName() const override;
+  void SetField(const std::string &field_name);
+  void SetResultName(const std::string name);
+
+protected:
+  void PreExecute() override;
+  void PostExecute() override;
+  void DoExecute() override;
+  
+  std::string m_field_name;
+  std::string m_out_name;
+};
+
+} //namespace vtkh
+#endif
