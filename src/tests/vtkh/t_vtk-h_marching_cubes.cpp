33557f82cac5aa2544ff70e74d4a9177df2a081b
Matt Larsen <mclarsen@users.noreply.github.com>
2019-01-03 07:31:45 -0800

    removing skips for iso values not contained in data (#80)

--- src/tests/vtkh/t_vtk-h_marching_cubes.cpp
+++ src/tests/vtkh/t_vtk-h_marching_cubes.cpp
@@ -21,10 +21,10 @@
 TEST(vtkh_marching_cubes, vtkh_serial_marching_cubes)
 {
   vtkh::DataSet data_set;
- 
+
   const int base_size = 32;
-  const int num_blocks = 2; 
-  
+  const int num_blocks = 2;
+
   for(int i = 0; i < num_blocks; ++i)
   {
     data_set.AddDomain(CreateTestData(i, num_blocks, base_size), i);
@@ -32,7 +32,7 @@ TEST(vtkh_marching_cubes, vtkh_serial_marching_cubes)
 
   vtkh::MarchingCubes marcher;
   marcher.SetInput(&data_set);
-  marcher.SetField("point_data"); 
+  marcher.SetField("point_data");
 
   const int num_vals = 2;
   double iso_vals [num_vals];
@@ -50,32 +50,32 @@ TEST(vtkh_marching_cubes, vtkh_serial_marching_cubes)
   float bg_color[4] = { 0.f, 0.f, 0.f, 1.f};
   vtkm::rendering::Camera camera;
   camera.ResetToBounds(bounds);
-  vtkh::Render render = vtkh::MakeRender(512, 
-                                         512, 
-                                         camera, 
-                                         *iso_output, 
+  vtkh::Render render = vtkh::MakeRender(512,
+                                         512,
+                                         camera,
+                                         *iso_output,
                                          "iso",
-                                          bg_color);  
+                                          bg_color);
   vtkh::RayTracer tracer;
   tracer.SetInput(iso_output);
-  tracer.SetField("cell_data"); 
+  tracer.SetField("cell_data");
 
   vtkh::Scene scene;
   scene.AddRenderer(&tracer);
   scene.AddRender(render);
   scene.Render();
 
-  delete iso_output; 
+  delete iso_output;
 }
 
 //----------------------------------------------------------------------------
-TEST(vtkh_marching_cubes, vtkh_marching_cubes_recenter)
+TEST(vtkh_marching_cubes, vtkh_empty)
 {
   vtkh::DataSet data_set;
- 
+
   const int base_size = 32;
-  const int num_blocks = 2; 
-  
+  const int num_blocks = 2;
+
   for(int i = 0; i < num_blocks; ++i)
   {
     data_set.AddDomain(CreateTestData(i, num_blocks, base_size), i);
@@ -83,7 +83,57 @@ TEST(vtkh_marching_cubes, vtkh_marching_cubes_recenter)
 
   vtkh::MarchingCubes marcher;
   marcher.SetInput(&data_set);
-  marcher.SetField("cell_data"); 
+  marcher.SetField("point_data");
+
+  const int num_vals = 2;
+  double iso_vals [num_vals];
+  iso_vals[0] = -1; // ask for something that does not exist
+  iso_vals[1] = -2;
+
+  marcher.SetIsoValues(iso_vals, num_vals);
+  marcher.AddMapField("point_data");
+  marcher.AddMapField("cell_data");
+  marcher.Update();
+
+  vtkh::DataSet *iso_output = marcher.GetOutput();
+
+  vtkm::Bounds bounds = iso_output->GetGlobalBounds();
+  float bg_color[4] = { 0.f, 0.f, 0.f, 1.f};
+  vtkm::rendering::Camera camera;
+  camera.ResetToBounds(bounds);
+  vtkh::Render render = vtkh::MakeRender(512,
+                                         512,
+                                         camera,
+                                         *iso_output,
+                                         "iso",
+                                          bg_color);
+  vtkh::RayTracer tracer;
+  tracer.SetInput(iso_output);
+  tracer.SetField("cell_data");
+
+  vtkh::Scene scene;
+  scene.AddRenderer(&tracer);
+  scene.AddRender(render);
+  scene.Render();
+  delete iso_output;
+}
+
+//----------------------------------------------------------------------------
+TEST(vtkh_marching_cubes, vtkh_marching_cubes_recenter)
+{
+  vtkh::DataSet data_set;
+
+  const int base_size = 32;
+  const int num_blocks = 2;
+
+  for(int i = 0; i < num_blocks; ++i)
+  {
+    data_set.AddDomain(CreateTestData(i, num_blocks, base_size), i);
+  }
+
+  vtkh::MarchingCubes marcher;
+  marcher.SetInput(&data_set);
+  marcher.SetField("cell_data");
 
   const int num_vals = 1;
   double iso_vals [num_vals];
@@ -100,20 +150,20 @@ TEST(vtkh_marching_cubes, vtkh_marching_cubes_recenter)
   float bg_color[4] = { 0.f, 0.f, 0.f, 1.f};
   vtkm::rendering::Camera camera;
   camera.ResetToBounds(bounds);
-  vtkh::Render render = vtkh::MakeRender(512, 
-                                         512, 
-                                         camera, 
-                                         *iso_output, 
+  vtkh::Render render = vtkh::MakeRender(512,
+                                         512,
+                                         camera,
+                                         *iso_output,
                                          "iso_cell",
-                                          bg_color);  
+                                          bg_color);
   vtkh::RayTracer tracer;
   tracer.SetInput(iso_output);
-  tracer.SetField("point_data"); 
+  tracer.SetField("point_data");
 
   vtkh::Scene scene;
   scene.AddRenderer(&tracer);
   scene.AddRender(render);
   scene.Render();
 
-  delete iso_output; 
+  delete iso_output;
 }
