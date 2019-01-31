68f2030261240b57250ed67cff338a4136869a24
James <james@jameskress.com>
2019-01-31 12:46:46 -0500

    Adding particle advection test, adding float64 data to test data set.

--- src/tests/vtkh/t_vtk-h_empty_data.cpp
+++ src/tests/vtkh/t_vtk-h_empty_data.cpp
@@ -25,46 +25,46 @@
 TEST(vtkh_emtpy_data, vtkh_empty_vtkm)
 {
   vtkh::DataSet data_set;
- 
+
   const int base_size = 32;
-  const int num_blocks = 1; 
-  
+  const int num_blocks = 1;
+
   for(int i = 0; i < num_blocks; ++i)
   {
     data_set.AddDomain(CreateTestData(i, num_blocks, base_size), i);
   }
-  
+
   vtkh::IsoVolume iso;
- 
+
   // create a range that does not exist
   vtkm::Range iso_range;
   iso_range.Min = -100.;
   iso_range.Max = -10.;
   iso.SetRange(iso_range);
-  iso.SetField("point_data");
+  iso.SetField("point_data_Float32");
   iso.SetInput(&data_set);
   iso.Update();
 
   vtkh::DataSet *iso_output = iso.GetOutput();
 
-   
+
   const int num_vals = 2;
   double iso_vals [num_vals];
   iso_vals[0] = -1; // ask for something that does not exist
   iso_vals[1] = (float)base_size * (float)num_blocks * 0.5f;
-  
+
   vtkh::MarchingCubes marcher;
-  marcher.SetField("point_data"); 
+  marcher.SetField("point_data_Float32");
   marcher.SetInput(iso_output);
   marcher.SetIsoValues(iso_vals, num_vals);
-  marcher.AddMapField("point_data");
-  marcher.AddMapField("cell_data");
+  marcher.AddMapField("point_data_Float32");
+  marcher.AddMapField("cell_data_Float32");
   marcher.Update();
 
   vtkh::DataSet *contour_output = marcher.GetOutput();
 
   vtkh::Clip clipper;
-  
+
   vtkm::Bounds clip_bounds = contour_output->GetGlobalBounds();
   vtkm::Vec<vtkm::Float64, 3> center = clip_bounds.Center();
   clip_bounds.X.Max = center[0] + .5;
@@ -79,7 +79,7 @@ TEST(vtkh_emtpy_data, vtkh_empty_vtkm)
 
   vtkh::Threshold thresher;
   thresher.SetInput(clip_output);
-  thresher.SetField("point_data"); 
+  thresher.SetField("point_data_Float32");
 
   double upper_bound = 1.;
   double lower_bound = 0.;
@@ -95,24 +95,24 @@ TEST(vtkh_emtpy_data, vtkh_empty_vtkm)
   camera.ResetToBounds(bounds);
   camera.SetPosition(vtkm::Vec<vtkm::Float64,3>(16,-32,-32));
   float bg_color[4] = { 0.f, 0.f, 0.f, 1.f};
-  vtkh::Render render = vtkh::MakeRender(512, 
-                                         512, 
-                                         camera, 
-                                         *thresh_output, 
+  vtkh::Render render = vtkh::MakeRender(512,
+                                         512,
+                                         camera,
+                                         *thresh_output,
                                          "empty_vtkm",
-                                         bg_color);  
-   
+                                         bg_color);
+
   vtkh::Scene scene;
   scene.AddRender(render);
 
   vtkh::RayTracer tracer;
   tracer.SetInput(iso_output);
-  tracer.SetField("point_data"); 
+  tracer.SetField("point_data_Float32");
 
-  scene.AddRenderer(&tracer);  
+  scene.AddRenderer(&tracer);
   scene.Render();
- 
-  delete iso_output; 
+
+  delete iso_output;
   delete contour_output;
   delete clip_output;
   delete thresh_output;
@@ -122,28 +122,28 @@ TEST(vtkh_emtpy_data, vtkh_empty_vtkm)
 TEST(vtkh_emtpy_data, vtkh_empty_vtkh)
 {
   vtkh::DataSet data_set;
- 
+
   vtkh::IsoVolume iso;
- 
+
   // create a range that does not exist
   vtkm::Range iso_range;
   iso_range.Min = -100.;
   iso_range.Max = -10.;
   iso.SetRange(iso_range);
-  iso.SetField("point_data");
+  iso.SetField("point_data_Float32");
   iso.SetInput(&data_set);
   iso.Update();
 
   vtkh::DataSet *iso_output = iso.GetOutput();
 
-   
+
   const int num_vals = 2;
   double iso_vals [num_vals];
   iso_vals[0] = -1.0;
   iso_vals[1] = 1.0;
-  
+
   vtkh::MarchingCubes marcher;
-  marcher.SetField("point_data"); 
+  marcher.SetField("point_data_Float32");
   marcher.SetInput(iso_output);
   marcher.SetIsoValues(iso_vals, num_vals);
   marcher.AddMapField("point_data");
@@ -153,7 +153,7 @@ TEST(vtkh_emtpy_data, vtkh_empty_vtkh)
   vtkh::DataSet *contour_output = marcher.GetOutput();
 
   vtkh::Clip clipper;
-  
+
   vtkm::Bounds clip_bounds = contour_output->GetGlobalBounds();
   vtkm::Vec<vtkm::Float64, 3> center = clip_bounds.Center();
   clip_bounds.X.Max = center[0] + .5;
@@ -168,7 +168,7 @@ TEST(vtkh_emtpy_data, vtkh_empty_vtkh)
 
   vtkh::Threshold thresher;
   thresher.SetInput(clip_output);
-  thresher.SetField("point_data"); 
+  thresher.SetField("point_data_Float32");
 
   double upper_bound = 1.;
   double lower_bound = 0.;
@@ -184,24 +184,24 @@ TEST(vtkh_emtpy_data, vtkh_empty_vtkh)
   camera.ResetToBounds(bounds);
   camera.SetPosition(vtkm::Vec<vtkm::Float64,3>(16,-32,-32));
   float bg_color[4] = { 0.f, 0.f, 0.f, 1.f};
-  vtkh::Render render = vtkh::MakeRender(512, 
-                                         512, 
-                                         camera, 
-                                         *thresh_output, 
+  vtkh::Render render = vtkh::MakeRender(512,
+                                         512,
+                                         camera,
+                                         *thresh_output,
                                          "empty_vtkh",
-                                         bg_color);  
-   
+                                         bg_color);
+
   vtkh::Scene scene;
   scene.AddRender(render);
 
   vtkh::RayTracer tracer;
   tracer.SetInput(iso_output);
-  tracer.SetField("point_data"); 
+  tracer.SetField("point_data_Float32");
 
-  scene.AddRenderer(&tracer);  
+  scene.AddRenderer(&tracer);
   scene.Render();
- 
-  delete iso_output; 
+
+  delete iso_output;
   delete contour_output;
   delete clip_output;
   delete thresh_output;
