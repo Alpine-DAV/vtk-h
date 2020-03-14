19167a191150076e525fa94860a7a77fcd883666
Matt Larsen <larsen30@llnl.gov>
2020-03-14 12:53:29 -0700

    bypass vtkm to support 2d vec mag

--- src/vtkh/filters/VectorMagnitude.cpp
+++ src/vtkh/filters/VectorMagnitude.cpp
@@ -2,10 +2,33 @@
 #include <vtkh/filters/VectorMagnitude.hpp>
 
 #include <vtkh/vtkm_filters/vtkmVectorMagnitude.hpp>
+#include <vtkm/worklet/Magnitude.h>
+#include <vtkm/TypeList.h>
 
 namespace vtkh
 {
 
+namespace detail
+{
+
+struct VectorMagFunctor
+{
+  vtkm::cont::Field result;
+  std::string m_result_name;
+  vtkm::cont::Field::Association m_assoc;
+  template<typename T, vtkm::IdComponent Size, typename S>
+  void operator()(const vtkm::cont::ArrayHandle<vtkm::Vec<T,Size>,S> &array)
+  {
+
+     vtkm::cont::ArrayHandle<T> mag_res;
+     vtkm::worklet::DispatcherMapField<vtkm::worklet::Magnitude>()
+          .Invoke(array, mag_res);
+
+     result = vtkm::cont::Field(m_result_name, m_assoc, mag_res);
+  }
+};
+
+} // namespace detail
 VectorMagnitude::VectorMagnitude()
 {
 
@@ -55,11 +78,21 @@ void VectorMagnitude::DoExecute()
     vtkm::cont::DataSet dom;
     this->m_input->GetDomain(i, dom, domain_id);
 
-    vtkh::vtkmVectorMagnitude mag;
-    auto dataset = mag.Run(dom,
-                           m_field_name,
-                           m_out_name,
-                           this->GetFieldSelection());
+    vtkm::cont::Field field = dom.GetField(m_field_name);
+    detail::VectorMagFunctor mag_func;
+    mag_func.m_result_name = m_out_name;
+    mag_func.m_assoc = field.GetAssociation();
+    field.GetData().ResetTypes(vtkm::TypeListFloatVec()).CastAndCall(mag_func);
+    vtkm::cont::DataSet dataset = dom;
+    dataset.AddField(mag_func.result);
+    // The current vtkm vector mag does not support
+    // vec2s. One day we might be able to use it again
+    //
+    //vtkh::vtkmVectorMagnitude mag;
+    //auto dataset = mag.Run(dom,
+    //                       m_field_name,
+    //                       m_out_name,
+    //                       this->GetFieldSelection());
 
     m_output->AddDomain(dataset, domain_id);
   }
